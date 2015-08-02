cd0a2153f614f601af4e0aed17175d94b4b94783
Udit Chugh <uditvapi69@gmail.com>
2015-08-02 23:59:26 +0530

    Initial

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "userdata";
+
+$conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT USERNAME,PASSWORD FROM registeredusers WHERE USERNAME = '" . $username . "' && PASSWORD = '" . $password . "'"; 
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+	$sql2 = "SELECT * FROM registeredusers";
+	$result2 = $conn->query($sql2);
+	while($row = $result2->fetch_assoc()) {
+        echo " Name: " . $row["NAME"]. " Email: " . $row["EMAIL"]."<br>";
+    }
+}
+else
+   echo "Login Unsucessful";
+?>
---
+++ registration.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "userdata";
+
+$conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$username = $_POST['username'];
+$password =  $_POST['password'];
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$usernames = "SELECT USERNAME FROM registeredusers WHERE USERNAME = '" . $username . "'";
+
+$usernameresult = $conn->query($usernames);
+
+if ($usernameresult->num_rows > 0){
+    die("Sorry! Username is already taken!");
+
+}
+
+$sql = "INSERT INTO registeredusers (NAME, EMAIL, USERNAME, PASSWORD)
+VALUES ('$name','$email','$username','$password')";
+
+if($conn->query($sql) === TRUE) {
+	echo "Success";
+}
+
+?>
